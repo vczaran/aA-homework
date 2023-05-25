b57afd47b56ec6296c9bc7653b2dc34ea62b12a1
Viktoria Czaran <vczaran@gmail.com>
2023-05-24 20:55:48 -0400

    Completed fast-dance

--- W4D4/octopus-probs/octopus.rb
+++ W4D4/octopus-probs/octopus.rb
@@ -69,3 +69,14 @@ end
 
 # p slow_dance("up", tiles_array)
 # p slow_dance("right-down", tiles_array)
+
+tiles_hash = { "up" => 0, "right-up" => 1, "right" => 2,
+               "right-down" => 3, "down" => 4, "left-down" => 5,
+                "left" => 6, "left-up" => 7 }
+
+def fast_dance(dir, tiles)
+    tiles[dir]
+end
+
+# p fast_dance("up", tiles_hash)
+# p fast_dance("right-down", tiles_hash)
