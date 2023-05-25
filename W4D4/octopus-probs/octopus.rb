d2ac7a16840f83216d328648b3eadba81d14f81b
Viktoria Czaran <vczaran@gmail.com>
2023-05-24 20:51:25 -0400

    Completed slow-dance and clever-octo

--- W4D4/octopus-probs/octopus.rb
+++ W4D4/octopus-probs/octopus.rb
@@ -6,44 +6,66 @@ def sluggish_octo(arr)
     arr.each_with_index do |ele1, i|
         arr.each_with_index do |ele2, j|
             if j > i && ele2.length > ele2.length
-                longest_fish += ele2
-            # else
-            #     longest_fish = ""
+                longest_fish = ele2
             end
         end
     end
     longest_fish
 end
 
-p sluggish_octo(fish)
+# p sluggish_octo(fish)
 
-longer = Proc.new {}
+prc = Proc.new { |x, y| x.length <=> y.length }
 
-def dom_octo(arr)
+def dom_octo(arr, prc)
     return arr if arr.length <= 1
     
     mid = arr.length/2
     left = arr[0...mid]
     right = arr[mid..-1]
     
-    Array.merge(left.merge_sort(&prc), right.merge_sort(&prc), &prc)
+    merge(dom_octo(left, prc), dom_octo(right, prc), prc)
     
-      end
+end
     
-      private
-      def self.merge(left, right, &prc)
-        merged = []
+def merge(left, right, prc)
+    merged = []
     
-        until left.empty? || right.empty?
-          if prc.call(left, right) == -1
+    until left.empty? || right.empty?
+        if prc.call(left, right) <= -1
             merged << left.shift
-          else
+        else
             merged << right.shift
-          end
         end
-          merged + left + right
-    
-      end
     end
+         merged + left + right
+        return merged[0]
+end
+
+# p dom_octo(fish, prc)
+
+
+def clever_octo(fish)
+    longest_fish = fish[0]
+
+    fish.each do |ele|
+        if ele.length > longest_fish.length
+            longest_fish = ele
+        end
+    end
+    longest_fish
 
 end
+
+# p clever_octo(fish)
+
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", 
+               "left",  "left-up"]
+
+
+def slow_dance(dir, tiles)
+    tiles.each_with_index { |ele, i| return i if ele == dir }
+end
+
+# p slow_dance("up", tiles_array)
+# p slow_dance("right-down", tiles_array)
