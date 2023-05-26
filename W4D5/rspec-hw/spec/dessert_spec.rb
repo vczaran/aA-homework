6c505ab04076b2c2e04beae1237ed75b09667402
Viktoria Czaran <vczaran@gmail.com>
2023-05-25 21:53:16 -0400

    Completed all except #mix

--- W4D5/rspec-hw/spec/dessert_spec.rb
+++ W4D5/rspec-hw/spec/dessert_spec.rb
@@ -23,7 +23,7 @@ describe Dessert do
     end
 
     it "raises an argument error when given a non-integer quantity" do
-      expect {dessert.initialize("", "", "")}.to raise_error(ArgumentError)
+      expect {Dessert.new("", "", "")}.to raise_error(ArgumentError)
     end
     end
 
@@ -43,7 +43,8 @@ describe Dessert do
 
   describe "#eat" do
     it "subtracts an amount from the quantity" do
-      expect(dessert.eat.quantity).to eq(4)
+      dessert.eat(1)
+      expect(dessert.quantity).to eq(4)
     end
 
     it "raises an error if the amount is greater than the quantity" do
@@ -53,11 +54,15 @@ describe Dessert do
 
   describe "#serve" do
     it "contains the titleized version of the chef's name" do
-      expect(dessert.serve).to eq(chef.titleize)
+      allow(chef).to receive(:titleize).and_return("Chef Remy the Great Baker")
+      expect(brownie.serve).to eq("Chef Remy the Great Baker has made 5 cookies!")
     end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+  it "calls bake on the dessert's chef with the dessert passed in" do
+    expect(chef).to receive(:bake).with(dessert)
+    dessert.make_more
   end
+end
 end
