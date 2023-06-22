751a905c302e76eab3ae4305057b13ff300eb0d6
Viktoria Czaran <vczaran@gmail.com>
2023-06-21 21:39:53 -0400

    Intro to Canvas

--- W8D4/intro_to_canvas/canvas-practice.js
+++ W8D4/intro_to_canvas/canvas-practice.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Your code here
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "red";
+  ctx.fillRect(300, 100, 300, 100);
 });
