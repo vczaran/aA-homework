1d4be090905685c887579f2bbfe314f6523ef1ad
Viktoria Czaran <vczaran@gmail.com>
2023-06-26 20:57:03 -0400

    Working on adding to sf-places list

--- W9D2/vanilla-dom/script.js
+++ W9D2/vanilla-dom/script.js
@@ -17,7 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding SF places as list items
 
-  // Your code here
+
+  const sf-places = document.querySelector("#sf-places");
+  const firstLi = sf-places.children[0];
+  const lastPlace = document.querySelector("div > li");
+  firstLi.append("input");
+  ul.append("input");
+
+
 
   
   // adding new photos
