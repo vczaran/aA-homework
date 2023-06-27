34aeb577f08cce80bed57d4a4b39f50da32824e5
Viktoria Czaran <vczaran@gmail.com>
2023-06-26 20:45:17 -0400

    Initialized W9D2 hw folder

---
+++ W9D2/vanilla-dom/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // toggling restaurants
+
+  const toggleLi = (e) => {
+    const li = e.target;
+    if (li.className === "visited") {
+      li.className = "";
+    } else {
+      li.className = "visited";
+    }
+  };
+
+  document.querySelectorAll("#restaurants li").forEach((li) => {
+    li.addEventListener("click", toggleLi);
+  });
+
+
+  // adding SF places as list items
+
+  // Your code here
+
+  
+  // adding new photos
+
+  // Your code here
+});
