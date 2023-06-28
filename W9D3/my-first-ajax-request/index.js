fed0c1af24a93d937487911e7f2681e9dc0805c9
Viktoria Czaran <vczaran@gmail.com>
2023-06-27 21:52:49 -0400

    AJAX request functional

--- W9D3/my-first-ajax-request/index.js
+++ W9D3/my-first-ajax-request/index.js
@@ -1,3 +1,15 @@
 console.log("Hello from the console!");
 
-// Your code here
+fetch("https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bcb83c4b54aee8418983c2aff3073b3b")
+.then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw response;
+    }
+  })
+  .then(
+        response => (console.log(response))
+  );
+
+console.log("The AJAX request has been dispatched.");
